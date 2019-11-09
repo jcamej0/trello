982a15476284b643fd5da43a69d333fd903ae78f
Juan Camejo <jcamejob@gmail.com>
2019-11-09 19:10:34 +0100

    Added login action service

--- src/services/auth/actions.js
+++ src/services/auth/actions.js
@@ -1,13 +1,36 @@
+import endpoints from "../endpoints";
+
+const encodeFormData = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 const actions = {
-    setAuthToken: (store, authToken) => {
-        store.setState({
-            auth: {
-                authenticated: true,
-                authToken: authToken,
-            }
-        })
-    },
-}
+  login: async (store, { username, password }) => {
+    try {
+      const response = await fetch(endpoints.ACCESS.LOGIN, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encodeFormData({ username, password })
+      });
+
+      const userAuthToken = await response.text();
+
+      actions.setAuthToken(store, userAuthToken);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+  setAuthToken: (store, authToken) => {
+    store.setState({
+      auth: {
+        authenticated: true,
+        authToken: authToken
+      }
+    });
+  }
+};
 
 export default actions;
