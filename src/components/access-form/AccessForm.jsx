0615f90659647d6be79d7b145302348c705c13b5
Juan Camejo <jcamejob@gmail.com>
2019-11-10 19:29:05 +0100

    Changed Acess Form component

--- src/components/access-form/AccessForm.jsx
+++ src/components/access-form/AccessForm.jsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import theme from "./theme.module.scss";
 
-const AccessForm = ({onSubmit, label}) => {
-    const [username, changeUsername] = useState();
-    const [password, changePassword] = useState();
+const AccessForm = ({ onSubmit, label }) => {
+  const [username, changeUsername] = useState();
+  const [password, changePassword] = useState();
 
-    const handleNameChange = (event) => {
-        changeUsername(event.target.value);
-    };
+  const handleNameChange = event => {
+    changeUsername(event.target.value);
+  };
 
-    const handlePasswordChange = (event) => {
-        changePassword(event.target.value);
-    };
+  const handlePasswordChange = event => {
+    changePassword(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onSubmit({username, password});
-    };
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit({ username, password });
+  };
 
-  return(
-    <form onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={handleNameChange} />
-        <input type="password" value={password} onChange={handlePasswordChange} />
-        <input type="submit" value={label} />
+  return (
+    <form className={theme.form} onSubmit={handleSubmit}>
+      <input type="text" value={username} onChange={handleNameChange} />
+      <input type="password" value={password} onChange={handlePasswordChange} />
+      <button type="submit"> {label}</button>
     </form>
-  )  
+  );
 };
 
 export default AccessForm;
