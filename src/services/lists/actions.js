df2bfb66d305dd0ab7fe70a809e71d948098f369
Juan Camejo <jcamejob@gmail.com>
2019-11-10 19:26:55 +0100

    Added create and remove lists actions

--- src/services/lists/actions.js
+++ src/services/lists/actions.js
@@ -8,7 +8,7 @@ const actions = {
     store.setState({
       lists: {
         fetching: true,
-        lists: [store.state.lists.lists]
+        lists: []
       }
     });
     try {
@@ -17,7 +17,7 @@ const actions = {
 
       store.setState({
         lists: {
-          lists: [store.state.lists.lists, ...lists]
+          lists: [...lists]
         }
       });
 
@@ -27,7 +27,74 @@ const actions = {
     } catch (err) {
       console.log("err", err);
     }
-  }
+	},
+	create: async (store, name) => {
+		try {
+			const authToken = store.state.auth.authToken;
+			const response = await httpClient.post(endpoints.LISTS.CREATE, authToken, {name});
+			const createList = await response.json();
+	
+			store.setState({
+				lists: {
+					lists: [...store.state.lists.lists, createList]
+				}
+			})
+			return true;
+		} catch(err) {
+			console.log(err)
+		}
+
+	},
+	destroy: async (store, idList) => {
+		const authToken = store.state.auth.authToken;
+		const lists = store.state.lists.lists;
+		const tasks = store.state.tasks.tasks;
+		const tasksByList = tasks[idList];
+
+		if(tasksByList) {
+			try{
+				const responseTasks = await httpClient.destroy(endpoints.TASKS.DELETE, authToken, idList);
+				const responseList = await httpClient.destroy(endpoints.LISTS.DELETE, authToken, idList);
+				const deletedTasks = await responseTasks.text();
+			  const deletedList = await responseList.json();
+			delete tasks[idList]
+			const filteredLists = lists.filter(list => list.id !== idList);
+			store.setState({
+				tasks: {
+					tasks: {
+						...tasks
+					}
+				},
+				lists: {
+					lists: 
+						filteredLists
+					
+				}
+			})
+				return true;
+			} catch(err) {
+				console.log("err", err);
+			}
+		} else {
+			try {
+				const responseList = await httpClient.destroy(endpoints.LISTS.DELETE, authToken, idList);
+				const deletedList = await responseList.json();
+				const filteredLists = lists.filter(list => list.id !== idList);
+				store.setState({
+					lists: {
+						lists: 
+							filteredLists
+						
+					}
+				})
+				return true;
+			} catch(err) {
+
+			}
+			
+		}
+
+	}
 };
 
 export default actions;
