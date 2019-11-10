5ca6da1ad1a99a6a5f8804c9b87c8c8c64b5cd84
Juan Camejo <jcamejob@gmail.com>
2019-11-10 19:31:12 +0100

    Login adn register style and component changes

--- src/scenes/login/Login.jsx
+++ src/scenes/login/Login.jsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import useGlobalState from '../../state';
-import AccessForm from '../../components/access-form';
-import './theme.scss';
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import useGlobalState from "../../state";
+import AccessForm from "../../components/access-form";
+import "./theme.scss";
 
 const Login = () => {
-    const [globalState, globalActions] = useGlobalState();
-    const { auth: {
-        authenticated
-    }} = globalState;
+  const [globalState, globalActions] = useGlobalState();
+  const {
+    auth: { authenticated }
+  } = globalState;
 
-    const {auth: {
-        login
-    }} = globalActions;
+  const {
+    auth: { login }
+  } = globalActions;
 
-    const handleLogin = async ({ username, password }) => {
-        login({ username, password });
-    }
+  const handleLogin = async ({ username, password }) => {
+    login({ username, password });
+  };
 
-    return(
-        <div class="container">
-            <div class="login-container">
-                <h1>Login</h1>
-                <AccessForm onSubmit={handleLogin} label="Ingresar" />
-                <Link to="/signup">
-                    <p>Registrarse</p>
-                </Link>
-            </div>
-            { authenticated &&
-                <Redirect to='/dashboard' />
-            }
-        </div>
-    )
-
+  return (
+    <div class="container">
+      <div class="login-container">
+        <h1>Login</h1>
+        <AccessForm onSubmit={handleLogin} label="Ingresar" />
+        <Link to="/signup">
+          <p>Registrarse</p>
+        </Link>
+      </div>
+      {authenticated && <Redirect to="/dashboard" />}
+    </div>
+  );
 };
 
 export default Login;
--- src/scenes/signup/Signup.jsx
+++ src/scenes/signup/Signup.jsx
@@ -1,34 +1,36 @@
-import React, {useState} from 'react';
-import AccessForm from '../../components/access-form';
-import actions from '../../services/signup';
-import Spinner from '../../ui-library/loading-spinner';
-import './theme.scss';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import AccessForm from "../../components/access-form";
+import actions from "../../services/signup";
+import Spinner from "../../ui-library/loading-spinner";
+import "./theme.scss";
 
 const Signup = () => {
-	const [signupStatus, changeSignupStatus] = useState(false);
+  const [signupStatus, changeSignupStatus] = useState(false);
 
-	const handleSignup = async ({name, password}) => {
-		changeSignupStatus('isLoading');
-		const registerStatus = await actions.register({name, password});
-		if(registerStatus) {
-			changeSignupStatus(true);
-			return;
-		} else {
-			changeSignupStatus(false);
-		}
-	}
-	return(
-		<div className="container">
-			<div className="login-container">
-				<h1>Registro</h1>
-				<AccessForm onSubmit={handleSignup} label="Registrarse"/>
-				<p>Iniciar sesión</p>
-				{signupStatus === 'isLoading' &&
-					<Spinner opaque />
-				}
-			</div>
-		</div>
-	)
+  const handleSignup = async ({ name, password }) => {
+    changeSignupStatus("isLoading");
+    const registerStatus = await actions.register({ name, password });
+    if (registerStatus) {
+      changeSignupStatus(true);
+      return;
+    } else {
+      changeSignupStatus(false);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="login-container">
+        <h1>Registro</h1>
+        <AccessForm onSubmit={handleSignup} label="Registrarse" />
+        <Link to="/">
+          <p>Iniciar sesión</p>
+        </Link>
+
+        {signupStatus === "isLoading" && <Spinner opaque />}
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
