66179b70212d3153fbb3c72dd2395875e7cc78ee
Juan Camejo <jcamejob@gmail.com>
2019-11-10 07:36:31 +0100

    Combined states and merged with global state

--- src/state/combineStates.js
+++ src/state/combineStates.js
@@ -1,27 +1,40 @@
-import React from 'react';
-import useGlobalHook from './store';
-import auth from '../services/auth';
-import user from '../services/user';
+import React from "react";
+import useGlobalHook from "./store";
+import auth from "../services/auth";
+import user from "../services/user";
+import lists from "../services/lists";
+import tasks from "../services/tasks";
 
 const state = {
-    auth: {
-        ...auth.state,
-    },
-    user: {
-        ...user.state,
-    }
-
+  auth: {
+    ...auth.state
+  },
+  user: {
+    ...user.state
+  },
+  lists: {
+    ...lists.state
+  },
+  tasks: {
+    ...tasks.state
+  }
 };
 
 const actions = {
-    auth: {
-        ...auth.actions,
-    },
-    user: {
-        ...user.actions,
-    }
+  auth: {
+    ...auth.actions
+  },
+  user: {
+    ...user.actions
+  },
+  lists: {
+    ...lists.actions
+  },
+  tasks: {
+    ...tasks.actions
+  }
 };
 
-const useGlobalState = useGlobalHook(React, actions, state);
+const useGlobalState = useGlobalHook(React, state, actions);
 
 export default useGlobalState;
