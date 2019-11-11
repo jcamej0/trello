69dff936fd0ad6a24b44fca9f1e8b526ecb8ba64
Juan Camejo <jcamejob@gmail.com>
2019-11-11 01:42:04 +0100

    Dashboard improve and some bug fixes

--- src/scenes/dashboard/Dashboard.jsx
+++ src/scenes/dashboard/Dashboard.jsx
@@ -1,39 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import useGlobalState from "../../state";
 import Lists from "./components";
 import { NewList } from "./components";
+import { ReactComponent as Logout } from "../../ui-library/svg/logout.svg";
 import Spinner from "../../ui-library/loading-spinner";
 import theme from "./theme.module.scss";
 
 const Dashboard = () => {
   const [globalState, globalActions] = useGlobalState();
   const {
-    lists: { lists, fetching }
+    lists: { lists, fetching },
+    auth: { authenticated }
   } = globalState;
 
   const {
-    lists: { fetch, create }
+    lists: { fetch, create },
+    auth: { logout }
   } = globalActions;
 
   useEffect(() => {
     fetch();
-	}, []);
-	
+  }, []);
+
+  const handleLogout = () => {
+    logout();
+  };
 
   return (
     <React.Fragment>
       {fetching && <Spinner opaque />}
       <div className={theme.container}>
-        <nav className={`${theme.navbar} ${theme.app}`}>Trello</nav>
+        <nav className={`${theme.navbar} ${theme.app}`}>
+          <span>Trello</span>
+          <div onClick={handleLogout} className={theme.logout}>
+            <Logout />
+          </div>
+        </nav>
         <div className={theme["list-container"]}>
           {lists &&
-            lists.map(list => {
-              return <Lists id={list.id} name={list.name} />;
+            lists.map((list, index) => {
+              return <Lists id={list.id} key={index} name={list.name} />;
             })}
 
-          <NewList handleCreateList={create}/>
+          <NewList handleCreateList={create} />
         </div>
       </div>
+      {!authenticated && <Redirect to="/" />}}
     </React.Fragment>
   );
 };
