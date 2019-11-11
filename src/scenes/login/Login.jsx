d67fe5a037adc22cac39d82ed3ccb1a519f85f16
Juan Camejo <jcamejob@gmail.com>
2019-11-11 01:40:58 +0100

    Improve Login component

--- src/scenes/login/Login.jsx
+++ src/scenes/login/Login.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import useGlobalState from "../../state";
 import AccessForm from "../../components/access-form";
-import "./theme.scss";
+import theme from "./theme.module.scss";
 
 const Login = () => {
-  const [globalState, globalActions] = useGlobalState();
+	const [globalState, globalActions] = useGlobalState();
+	const [errorLoginMessage, changeErrorLoginMessage] = useState(false);
   const {
     auth: { authenticated }
   } = globalState;
@@ -13,16 +14,22 @@ const Login = () => {
   const {
     auth: { login }
   } = globalActions;
-
+	
   const handleLogin = async ({ username, password }) => {
-    login({ username, password });
+		const success = await login({ username, password });
+		if(!success) {
+			changeErrorLoginMessage(true);
+		}
   };
 
   return (
-    <div class="container">
-      <div class="login-container">
+    <div className={theme.container}>
+      <div className={theme['login-container']}>
         <h1>Login</h1>
         <AccessForm onSubmit={handleLogin} label="Ingresar" />
+				{ errorLoginMessage &&
+					<p className={theme['error-message']}>Error, usuario no encontrado.</p>
+				}
         <Link to="/signup">
           <p>Registrarse</p>
         </Link>
